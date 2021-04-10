fab13a2565f93f99df389ab0449af53e9e228dd8
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2021-04-10 14:17:10 +0200

    add todos, delete todos, basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,22 @@ const App: React.FC = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
 
   const addTodoHandler = (text: string) => {
-    setTodos([{ id: Math.random().toString(), text: text }]);
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: Math.random().toString(), text: text },
+    ]);
+  };
+
+  const deleteTodoHandler = (todoId: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== todoId);
+    });
   };
 
   return (
     <div className="App">
       <NewTodo addTodo={addTodoHandler} />
-      <TodoList items={todos} />
+      <TodoList items={todos} deleteTodo={deleteTodoHandler} />
     </div>
   );
 };
